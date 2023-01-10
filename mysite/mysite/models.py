c4ef0dd71610a2ce7a76647f14381bb20b722908
Parul kumar <Parulkumar@cons.com>
2023-01-10 19:16:54 +0530

    1st commit

--- mysite/mysite/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-class salservercon(models.Model):
-    employeeID=models.IntegerField(max_length=100)
-    LastName=models.CharField(max_length=100)
-    FirstName =models.CharField(max_length=100)
-    City= models.CharField(max_length=100)
